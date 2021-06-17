a178776b9990015db146296913f47218d5087df4
TenDan <tytusworld@gmail.com>
2021-06-17 21:03:14 +0200

    Dodaj pliki projektów.

---
+++ Entities/FlatsDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MySql.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace FlatsAPI.Entities
+{
+    public class FlatsDbContext : DbContext
+    {
+        private readonly IConfiguration Configuration;
+        public FlatsDbContext(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseMySQL(Configuration["ConnectionStrings:Default"]);
+        }
+    }
+}
