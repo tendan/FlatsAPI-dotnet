184dc8a9e4987412b2ceb5169f69bc03470d9da7
TenDan <tytusworld@gmail.com>
2021-06-19 13:07:15 +0200

    Simple delete FlatId column migration

---
+++ Migrations/20210619110433_RemoveFlatIdColumnFromAccount.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlatsAPI.Migrations
+{
+    public partial class RemoveFlatIdColumnFromAccount : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "FlatId",
+                table: "Accounts");
+        }
+    }
+}
