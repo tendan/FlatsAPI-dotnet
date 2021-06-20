9a09652e4eedf6f99a92314b913dfcc9eebb7f38
TenDan <tytusworld@gmail.com>
2021-06-20 13:25:29 +0200

    Implement the FlatInRentDto and simplify the FlatInBlockOfFlatsDto

--- Models/FlatInRentDto.cs
+++ Models/FlatInRentDto.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FlatsAPI.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -7,5 +8,15 @@ namespace FlatsAPI.Models
 {
     public class FlatInRentDto
     {
+        public int Id { get; set; }
+        public int Area { get; set; }
+        public int Number { get; set; }
+        public int NumberOfRooms { get; set; }
+        public int Floor { get; set; }
+
+        public OwnerShip? OwnerShip { get; set; }
+
+        public float PriceWhenBought { get; set; }
+        public float? PricePerMeterSquaredWhenRented { get; set; }
     }
 }
