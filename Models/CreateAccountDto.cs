824dd6b3f7ffcc9a5e91652eea12b99434b4e9d4
TenDan <tytusworld@gmail.com>
2021-06-24 15:44:10 +0200

    Update CreateAccountDto to make role field.

--- Models/CreateAccountDto.cs
+++ Models/CreateAccountDto.cs
@@ -13,5 +13,6 @@ namespace FlatsAPI.Models
         public string LastName { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
+        public int? RoleId { get; set; }
     }
 }
