b35302d32410eea84b9b0256164347e0c11869c6
TenDan <tytusworld@gmail.com>
2021-06-25 19:20:21 +0200

    Move columnSelectors Dictionary to separate type

---
+++ Models/ColumnSelector.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace FlatsAPI.Models
+{
+    public class ColumnSelector<T> : Dictionary<string, Expression<Func<T, object>>>
+    {
+    }
+}
