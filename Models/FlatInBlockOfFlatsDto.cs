d58acde33f05ee522cb722cd06b4086022621743
TenDan <tytusworld@gmail.com>
2021-06-25 19:08:07 +0200

    Recovered the FlatInBlockOfFlatsDto

---
+++ Models/FlatInBlockOfFlatsDto.cs
@@ -0,0 +1,27 @@
+﻿using FlatsAPI.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlatsAPI.Models
+{
+    public class FlatInBlockOfFlatsDto
+    {
+        public int Id { get; set; }
+        public int Area { get; set; }
+        public int Number { get; set; }
+        public int NumberOfRooms { get; set; }
+        public int Floor { get; set; }
+
+        public int? OwnerId { get; set; }
+        public virtual AccountDto Owner { get; set; }
+
+        public virtual ICollection<AccountDto> Tenants { get; set; }
+
+        public OwnerShip? OwnerShip { get; set; }
+
+        public float PriceWhenBought { get; set; }
+        public float? PricePerMeterSquaredWhenRented { get; set; }
+    }
+}
