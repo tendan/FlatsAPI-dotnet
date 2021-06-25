7003314bb0417b0c6f514b4dd970c0249818def7
TenDan <tytusworld@gmail.com>
2021-06-25 19:13:29 +0200

    Migrations recreate

--- Migrations/20210625165409_RecreateTables.Designer.cs
+++ Migrations/20210625165409_RecreateTables.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using FlatsAPI.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FlatsAPI.Migrations
 {
     [DbContext(typeof(FlatsDbContext))]
-    partial class FlatsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210625165409_RecreateTables")]
+    partial class RecreateTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
