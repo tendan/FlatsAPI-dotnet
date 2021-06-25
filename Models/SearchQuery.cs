c6bc231b5fdc0d9053ef66daae438964cba965bb
TenDan <tytusworld@gmail.com>
2021-06-25 19:20:02 +0200

    Add default values for SearchQuery

--- Models/SearchQuery.cs
+++ Models/SearchQuery.cs
@@ -8,8 +8,8 @@ namespace FlatsAPI.Models
     public class SearchQuery
     {
         public string SearchPhrase { get; set; }
-        public int PageNumber { get; set; }
-        public int PageSize { get; set; }
+        public int PageNumber { get; set; } = 1;
+        public int PageSize { get; set; } = 5;
         public string SortBy { get; set; }
         public SortDirection SortDirection { get; set; }
     }
