0dbe9f3e25155f7e1bdc53b87b0f5d16b2b31abd
TenDan <tytusworld@gmail.com>
2021-06-29 22:46:45 +0200

    Add BadRequestException and add handle for it

---
+++ Exceptions/BadRequestException.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace FlatsAPI.Exceptions
+{
+    public class BadRequestException : Exception
+    {
+        public BadRequestException()
+        {
+        }
+
+        public BadRequestException(string message) : base(message)
+        {
+        }
+
+        public BadRequestException(string message, Exception innerException) : base(message, innerException)
+        {
+        }
+
+        protected BadRequestException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+    }
+}
--- Middleware/ErrorHandlingMiddleware.cs
+++ Middleware/ErrorHandlingMiddleware.cs
@@ -20,6 +20,11 @@ namespace FlatsAPI.Middleware
                 context.Response.StatusCode = 404;
                 await context.Response.WriteAsync(notFoundException.Message);
             }
+            catch (BadRequestException badRequestException)
+            {
+                context.Response.StatusCode = 400;
+                await context.Response.WriteAsync(badRequestException.Message);
+            }
             catch (Exception e)
             {
                 context.Response.StatusCode = 500;
