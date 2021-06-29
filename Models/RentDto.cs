14f655d80234107449568a61fb021fb0c661f6af
TenDan <tytusworld@gmail.com>
2021-06-29 22:46:09 +0200

    Modify RentDto

--- Models/RentDto.cs
+++ Models/RentDto.cs
@@ -12,12 +12,12 @@ namespace FlatsAPI.Models
         public bool Paid { get; set; }
         public float Price { get; set; }
         public float PriceWithTax { get; set; }
-        public int FlatId { get; set; }
-        public virtual FlatInRentDto flat { get; set; }
-        /*
+        /*public int FlatId { get; set; }
+        public virtual FlatInRentDto flat { get; set; }*/
+
         public virtual AccountDto Owner { get; set; }
 
         public virtual ICollection<AccountDto> Tenants { get; set; }
-        */
+
     }
 }
