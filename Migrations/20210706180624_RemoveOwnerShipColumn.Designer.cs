c2059fc9dabf4459aab22bb7f4d4907a56e491f0
TenDan <tytusworld@gmail.com>
2021-07-07 12:52:29 +0200

    Apply migration

--- Migrations/20210706180624_RemoveOwnerShipColumn.Designer.cs
+++ Migrations/20210706180624_RemoveOwnerShipColumn.Designer.cs
@@ -3,14 +3,16 @@ using System;
 using FlatsAPI.Entities;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace FlatsAPI.Migrations
 {
     [DbContext(typeof(FlatsDbContext))]
-    partial class FlatsDbContextModelSnapshot : ModelSnapshot
+    [Migration("20210706180624_RemoveOwnerShipColumn")]
+    partial class RemoveOwnerShipColumn
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Migrations/20210706180624_RemoveOwnerShipColumn.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace FlatsAPI.Migrations
+{
+    public partial class RemoveOwnerShipColumn : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "OwnerShip",
+                table: "Flats");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "OwnerShip",
+                table: "Flats",
+                type: "int",
+                nullable: true);
+        }
+    }
+}
