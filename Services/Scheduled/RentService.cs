8c17715e6b812c33b86f931660ed666c94c21020
TenDan <tytusworld@gmail.com>
2021-07-08 17:42:32 +0200

    Hosted services as scheduled tasks first try

---
+++ Services/Scheduled/RentService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace FlatsAPI.Services.Scheduled
+{
+    public class RentService : IHostedService
+    {
+        private Timer _timer;
+
+        public Task StartAsync(CancellationToken cancellationToken)
+        {
+            _timer = new Timer(GenerateRents, null, TimeSpan.Zero, TimeSpan.FromSeconds(10));
+
+            return Task.CompletedTask;
+        }
+        public Task StopAsync(CancellationToken cancellationToken)
+        {
+            _timer?.Change(Timeout.Infinite, 0);
+
+            return Task.CompletedTask;
+        }
+
+        public void GenerateRents(object state)
+        {
+            Console.WriteLine("Scheduled task test");
+        }
+    }
+}
