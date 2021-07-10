5de7511be40dab969c133f33c723b946c6e7b04f
TenDan <tytusworld@gmail.com>
2021-07-10 13:58:17 +0200

    Add services for handling invoices and rents

---
+++ Services/InvoiceService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlatsAPI.Services
+{
+    public interface IInvoiceService
+    {
+        byte[] GetInvoiceForSpecifiedAccount(int accountId);
+    }
+    public class InvoiceService : IInvoiceService
+    {
+        public byte[] GetInvoiceForSpecifiedAccount(int accountId)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/RentService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlatsAPI.Services
+{
+    public interface IRentService
+    {
+
+    }
+    public class RentService : IRentService
+    {
+    }
+}
