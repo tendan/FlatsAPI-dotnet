f25383cebecbacd976953e28377a6577619048d8
TenDan <tytusworld@gmail.com>
2021-07-11 12:28:05 +0200

    Add PaymentSettings constants

---
+++ Settings/PaymentSettings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlatsAPI.Settings
+{
+    public class PaymentSettings
+    {
+        public const float TAX = 1.23F;
+        public const float RENTED_SPLIT_UP = 15;
+    }
+}
