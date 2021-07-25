0bb2ba1bd6664c8d249549d91e08851ee71a60d9
TenDan <tytusworld@gmail.com>
2021-07-25 12:21:22 +0200

    Begin work with InvoiceService implementation

--- Services/InvoiceService.cs
+++ Services/InvoiceService.cs
@@ -1,5 +1,12 @@
-﻿using System;
+﻿using FlatsAPI.Entities;
+using iText.Kernel.Geom;
+using iText.Kernel.Pdf;
+using iText.Layout;
+using iText.Layout.Properties;
+using iText.Layout.Element;
+using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -11,9 +18,48 @@ namespace FlatsAPI.Services
     }
     public class InvoiceService : IInvoiceService
     {
+        private readonly FlatsDbContext _dbContext;
+
+        public InvoiceService(FlatsDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
         public byte[] GetInvoiceForSpecifiedAccount(int accountId)
         {
             throw new NotImplementedException();
+        }
+        private void GeneratePdf(int accountId)
+        {
+            var stream = new MemoryStream();
+            var writer = new PdfWriter(stream);
+            var pdf = new PdfDocument(writer);
+            var document = new Document(pdf);
+        }
+        private Table GenerateRentsTable()
+        {
+            var table = new Table(4, false);
+
+            table.SetHorizontalAlignment(HorizontalAlignment.RIGHT);
+            table.SetWidth(UnitValue.CreatePercentValue(50));
+
+            var tableHeader1 = new Cell(1, 1)
+                .SetTextAlignment(TextAlignment.LEFT)
+                .Add(new Paragraph("Stawka VAT"));
+
+            var tableHeader2 = new Cell(1, 1)
+                .SetTextAlignment(TextAlignment.LEFT)
+                .Add(new Paragraph("Netto"));
+
+            var tableHeader3 = new Cell(1, 1)
+                .SetTextAlignment(TextAlignment.LEFT)
+                .Add(new Paragraph("VAT"));
+
+            var tableHeader4 = new Cell(1, 1)
+                .SetTextAlignment(TextAlignment.LEFT)
+                .Add(new Paragraph("Brutto"));
+
+            return table;
         }
     }
 }
