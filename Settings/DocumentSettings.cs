dcc9a5790eb2164cf206032a2339b872d292c7fe
TenDan <tytusworld@gmail.com>
2021-07-26 14:20:09 +0200

    Add DocumentSettings.cs

---
+++ Settings/DocumentSettings.cs
@@ -0,0 +1,17 @@
+﻿using iText.IO.Font;
+using iText.IO.Font.Constants;
+using iText.Kernel.Font;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlatsAPI.Settings
+{
+    public class DocumentSettings
+    {
+        public PdfFont PrimaryFont { get; } = PdfFontFactory.CreateFont(StandardFonts.HELVETICA, PdfEncodings.UTF8);
+        public PdfFont SecondaryFont { get; } = PdfFontFactory.CreateFont(StandardFonts.HELVETICA_OBLIQUE, PdfEncodings.UTF8);
+    }
+}
