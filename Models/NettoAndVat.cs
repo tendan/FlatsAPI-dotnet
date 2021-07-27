fe3be20262a4273af6a54ccc4bb907bf1f2d9b37
TenDan <tytusworld@gmail.com>
2021-07-27 15:32:00 +0200

    Add NettoAndVat helper model

---
+++ Models/NettoAndVat.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlatsAPI.Models
+{
+    public class NettoAndVat
+    {
+        public float NettoSummary { get; set; }
+        public float VatSummary { get; set; }
+    }
+}
