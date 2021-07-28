47af43e890ab050d9f01c866475254fb85bf756a
TenDan <tytusworld@gmail.com>
2021-07-28 15:47:48 +0200

    Add PermissionAuthorize documentation

--- Authorization/PermissionAuthorizeAttribute.cs
+++ Authorization/PermissionAuthorizeAttribute.cs
@@ -6,11 +6,20 @@ using System.Threading.Tasks;
 
 namespace FlatsAPI.Authorization
 {
+    /// <summary>
+    /// Authorizes user by required permission
+    /// </summary>
     public class PermissionAuthorizeAttribute : AuthorizeAttribute
     {
         const string POLICY_PREFIX = "Permission";
         private string _permission;
 
+        /// <summary>
+        /// Describe what permission is required for specified endpoint
+        /// 
+        /// It is recommended to use enumerable type in Settings\Permissions
+        /// </summary>
+        /// <param name="permission"></param>
         public PermissionAuthorizeAttribute(string permission) => Permission = permission;
 
         public string Permission
