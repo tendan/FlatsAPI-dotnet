8c36af125dffb6aa49f727458f837856fea06357
TenDan <tytusworld@gmail.com>
2021-07-29 13:49:54 +0200

    Add UpdateAccountDto

---
+++ Models/UpdateAccountDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlatsAPI.Models
+{
+    public class UpdateAccountDto
+    {
+        public string? Email { get; set; }
+        public string? Username { get; set; }
+        public string? FirstName { get; set; }
+        public string? LastName { get; set; }
+        public string? Password { get; set; }
+        public string? ConfirmPassword { get; set; }
+        public string? BillingAddress { get; set; }
+        public string? PhoneNumber { get; set; }
+    }
+}
