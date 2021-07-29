dd2dbd12c7d5fdff4256e0f1f1e391cc589da2e2
TenDan <tytusworld@gmail.com>
2021-07-29 13:50:14 +0200

    Add PhoneNumber validator

---
+++ Models/Validators/MyValidatorExtensions.cs
@@ -0,0 +1,17 @@
+﻿using FlatsAPI.Models.Validators.PropertyValidators;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FlatsAPI.Models.Validators
+{
+    public static class MyValidatorExtensions
+    {
+        public static IRuleBuilderOptions<T, TElement> PhoneNumber<T, TElement>(this IRuleBuilder<T, TElement> ruleBuilder)
+        {
+            return ruleBuilder.SetValidator(new PhoneNumberValidator<T, TElement>());
+        }
+    }
+}
---
+++ Models/Validators/PropertyValidators/PhoneNumberValidator.cs
@@ -0,0 +1,38 @@
+﻿using FluentValidation;
+using FluentValidation.Validators;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace FlatsAPI.Models.Validators.PropertyValidators
+{
+    public class PhoneNumberValidator<T, TElement> : PropertyValidator<T, TElement>
+    {
+        public override string Name => "PhoneNumberValidator";
+
+        public PhoneNumberValidator() { }
+
+        public override bool IsValid(ValidationContext<T> context, TElement value)
+        {
+            var pattern
+                = "^(\\+\\d{1,3}( )?)?((\\(\\d{3}\\))|\\d{3})[- .]?\\d{3}[- .]?\\d{4}$"
+                + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?){2}\\d{3}$"
+                + "|^(\\+\\d{1,3}( )?)?(\\d{3}[ ]?)(\\d{2}[ ]?){2}\\d{2}$";
+
+            var phoneNumberRegex = new Regex(pattern);
+
+            if (value is not null)
+            {
+                if (!phoneNumberRegex.IsMatch(value.ToString()))
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
+        protected override string GetDefaultMessageTemplate(string errorCode)
+        => "{PropertyName} must be a phone number";
+    }
+}
