bedceb6c006fcb81aea726fcd527737a33f26077
TenDan <tytusworld@gmail.com>
2021-07-29 12:37:14 +0200

    Add new fields to CreateAccountDto (BillingAddress, PhoneNumber) (both are optional)

--- Models/CreateAccountDto.cs
+++ Models/CreateAccountDto.cs
@@ -13,6 +13,9 @@ namespace FlatsAPI.Models
         public string LastName { get; set; }
         public string Password { get; set; }
         public string ConfirmPassword { get; set; }
+        public DateTime DateOfBirth { get; set; }
+        public string? BillingAddress { get; set; }
+        public string? PhoneNumber { get; set; }
         public int? RoleId { get; set; }
     }
 }
