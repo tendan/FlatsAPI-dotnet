dbfeb40ce75bd27fc101b2a4c73abdba7c22b221
TenDan <tytusworld@gmail.com>
2021-07-30 15:36:13 +0200

    Add Header Cell background color constant

--- Settings/DocumentSettings.cs
+++ Settings/DocumentSettings.cs
@@ -1,5 +1,6 @@
 ﻿using iText.IO.Font;
 using iText.IO.Font.Constants;
+using iText.Kernel.Colors;
 using iText.Kernel.Font;
 using System;
 using System.Collections.Generic;
@@ -16,5 +17,6 @@ namespace FlatsAPI.Settings
         private static string LiberationSansFontPath = CurrentDirectory + @"\Static\Fonts\LiberationSans.ttf";
         public static PdfFont PrimaryFont { get; } = PdfFontFactory.CreateFont(LiberationSansFontPath, PdfEncodings.IDENTITY_H);
         public static PdfFont SecondaryFont { get; } = PdfFontFactory.CreateFont(LiberationSansFontPath, PdfEncodings.IDENTITY_H);
+        public static Color TableHeaderCellColor { get; } = new DeviceRgb(220, 220, 220);
     }
 }
