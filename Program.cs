b669499b9d1473f0e40f50c4fee4de4f26d43f7d
TenDan <tytusworld@gmail.com>
2022-08-28 16:16:31 +0200

    Change the name of an API in Swagger

--- Program.cs
+++ Program.cs
@@ -100,7 +100,7 @@ if (app.Environment.IsDevelopment())
 {
     app.UseDeveloperExceptionPage();
     app.UseSwagger();
-    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "TodoApi v1"));
+    app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "Flats Of Blocks v1.0"));
 }
 
 app.UseMiddleware<ErrorHandlingMiddleware>();
